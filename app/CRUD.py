a3e3f317c8dc4f7c3464f2dad1584f22545c248b
Clay Cardenas <ogclaystick@gmail.com>
2023-04-03 23:12:48 -0500

    CRUD Testing Database from admin panel

--- app/CRUD.py
+++ app/CRUD.py
@@ -1,2 +1,30 @@
-#crud commands here
+#crud databse commands here
+from flask import jsonify, request, render_template
+
+from app import app, db
+from app.models import Events
+
+#event CRUD commands
+# Create a new event
+
+@app.route('/events', methods=['POST'])
+def create_event():
+    title = request.form['title']
+    summary = request.form['summary']
+    presenter = request.form['presenter']
+    place = request.form['place']
+    time = request.form['time']
+    date = request.form['date']
+    event = Events(title=title, summary=summary, presenter=presenter, place=place, time=time, date=date)
+    db.session.add(event)
+    db.session.commit()
+    return jsonify({'message': 'Event created successfully!'})
+
+# load all events into website
+@app.route('/events', methods=['GET'])
+def get_events():
+    events = Events.query.all()
+    return render_template('admin.html', events=events)
+
+
 
--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,16 @@ from flask import render_template, flash, redirect, url_for
 from app.forms import SignupForm, EmailError
 import random
 from app import db
+
 from app.models import  UserPassword, UserAccount, Staff, Trending, News, Events
+
+
+
 
 with app.app_context():
     db.create_all()
+
+from app.CRUD import *
 
 """
 fake_events = [    {        'date': {'day': '26', 'month': 'Jun'},        'title': 'Robotathon',        'location': 'A300 UHD Main campus',        'contact_number': '0905136250',        'contact_email': 'longhai2511@gmail.com'    },
@@ -45,6 +51,9 @@ def staff():
     staff_members = Staff.query.limit(3).all()
     return render_template('staff.html', staff_members=staff_members)
 
+@app.route('/admin')
+def loadPage():
+    return render_template('admin.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
